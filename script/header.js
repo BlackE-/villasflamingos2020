31897cce3b0e4b6eaefa556dc0d24fe6ccfe4ed5
BlackE <elizabeth.ramescamilla@gmail.com>
2020-10-09 02:07:11 -0500

    fix(style): header update

---
+++ script/header.js
@@ -0,0 +1,15 @@
+	console.log('%cBy @Studio-SUB', 'font-size: 14px;color: #000; border:1px solid #000;');
+	/*		Start of Zopim Live Chat Script 		*/
+    window.$zopim||(function(d,s){var z=$zopim=function(c){
+    z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+    $.src='//v2.zopim.com/?2WmvVkvK1FGKPGUqNrTgg2BqyfwgapU9';z.t=+new Date;$.
+    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+
+    
+    $zopim(function() {
+        $zopim.livechat.setLanguage('es');
+    });
+
+    /* 		End of Zopim Live Chat script 	*/
